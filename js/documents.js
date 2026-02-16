9cd807e00145ed88f9fa21710957ac96e2b725f0
Nishad018 <nishad.karande24@spit.ac.in>
2026-02-16 18:51:34 +0000

    final changes i think

--- js/documents.js
+++ js/documents.js
@@ -1,19 +1,56 @@
+/* ---------------- NORMAL DOCUMENTS (Finance Page) ---------------- */
+
 function addDocument(){
 
-let container=document.getElementById("docContainer");
+let container = document.getElementById("docContainer");
 
-let div=document.createElement("div");
+let div = document.createElement("div");
 
-div.innerHTML=`
+div.innerHTML = `
 <select>
 <option>Bank Statement</option>
 <option>Utility Bill</option>
 <option>Rent Receipt</option>
 <option>Invoice</option>
+<option>UPI Transaction History</option>
 <option>Other</option>
 </select>
 
-<input type="file">
+<input type="file" accept=".pdf,.jpg,.png,.doc,.docx">
+<br><br>
+`;
+
+container.appendChild(div);
+
+}
+
+
+/* ---------------- COLLATERAL DOCUMENTS ---------------- */
+
+function addCollateral(){
+
+let container = document.getElementById("docContainer");
+
+let div = document.createElement("div");
+
+div.innerHTML = `
+<select>
+<option>Land Papers</option>
+<option>House Property Papers</option>
+<option>Farm Land Records (7/12 Extract)</option>
+<option>Machinery Ownership Proof</option>
+<option>Agricultural Equipment</option>
+<option>Cattle Ownership Proof</option>
+<option>Tractor / Vehicle Ownership</option>
+<option>Job / Employment Proof</option>
+<option>Gold / Jewellery Proof</option>
+<option>Village Panchayat Certificate</option>
+<option>Self Help Group Proof</option>
+<option>Other Collateral</option>
+</select>
+
+<input type="file" accept=".pdf,.jpg,.png,.doc,.docx">
+<br><br>
 `;
 
 container.appendChild(div);
