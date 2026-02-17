42e36fbaad52392a4a4c41fc42c33f0fa3caee49
Nishad018 <nishad.karande24@spit.ac.in>
2026-02-17 07:20:46 +0000

    progressbar

--- js/navigation.js
+++ js/navigation.js
@@ -1,3 +1,22 @@
 function goNext(page){
 window.location.href=page;
 }
+function updateProgress(currentPage) {
+    const pages = [
+        'index.html',           // Login
+        'verification.html',    // Verification
+        'userType.html',        // User Type
+        'finance.html',         // Finance
+        'collateral.html',      // Collateral
+        'social.html',          // Social
+        'summary.html'          // Summary
+    ];
+
+    const progressBar = document.getElementById("progressBar");
+    const index = pages.indexOf(currentPage);
+
+    if(index !== -1) {
+        const progressPercent = ((index + 1) / pages.length) * 100;
+        progressBar.style.width = progressPercent + "%";
+    }
+}
